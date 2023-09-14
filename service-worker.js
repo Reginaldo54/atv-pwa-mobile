4e58847745101ef001aac7ab7cdd1dde6080894d
Reginaldo Alves <37334200+Reginaldo54@users.noreply.github.com>
2023-09-14 00:09:50 -0300

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-var cacheName = 'atvpwa+-v1.0';
+var cacheName = 'atvPwa+-v1.0';
 
 self.addEventListener('install', event => {
 
@@ -6,73 +6,94 @@ self.addEventListener('install', event => {
 
   event.waitUntil(
     caches.open(cacheName)
-      .then(cache => cache.addAll([
+      .then(async cache => {
+        const resources = [
+          'index.html',
 
-        './index.html',
+          'assets/css/fontawesome-all.min.css',
+          
+          'assets/css/main.css',
+  
+          'assets/css/images/arrow.svg',
+  
+          'assets/css/images/overlay.png',
+  
+          'assets/js/breakpoints.min.js',
+  
+          'assets/js/browser.min.js',
+  
+          'assets/js/jquery.min.js',
+          
+          'assets/js/jquery.scrollex.min.js',
+          
+          'assets/js/jquery.scrolly.min.js',
+  
+          'assets/js/main.js',
+          
+          'assets/js/util.js',
+  
+          'assets/js/popper.min.js',
+  
+          'assets/sass/main.scss',
+  
+          'assets/sass/libs/_breakpoints.scss',
+          'assets/sass/libs/_functions.scss',
+          'assets/sass/libs/_html-grid.scss',
+          'assets/sass/libs/_mixins.scss',
+          'assets/sass/libs/_vars.scss',
+          'assets/sass/libs/_vendor.scss',
+  
+          'assets/webfonts/fa-brands-400.eot',
+          'assets/webfonts/fa-brands-400.svg',
+          'assets/webfonts/fa-brands-400.ttf',
+          'assets/webfonts/fa-brands-400.woff',
+          'assets/webfonts/fa-brands-400.woff2',
+          
+          'assets/webfonts/fa-regular-400.eot',
+          'assets/webfonts/fa-regular-400.svg',
+          'assets/webfonts/fa-regular-400.ttf',
+          'assets/webfonts/fa-regular-400.woff',
+          'assets/webfonts/fa-regular-400.woff2',
+  
+          'icons/128.png',
+          'icons/144.png',
+          'icons/152.png',
+          'icons/167.png',
+          'icons/180.png',
+          'icons/192.png',
+          'icons/256.png',
+          'icons/512.png',
+          'icons/1024.png',
+  
+          'images/bg.jpg',
+          'images/pic01.jpg',
+          'images/pic02.jpg',
+          'images/pic03.jpg',
+          'images/pic04.jpg',
+          'images/pic05.jpg',
+  
+        ];
 
-        './assets/css/fontawesome-all.min.css',
-        
-        './assets/css/main.css',
+        const addedResources = await Promise.all(
+          resources.map(async resource => {
+            try {
+              const response = await fetch(resource);
+              if (!response.ok) {
+                throw new Error(`Request failed for ${resource}`);
+              }
+              await cache.put(resource, response.clone());
+              return resource;
+            } catch (error) {
+              console.error(error);
+              return null;
+            }
+          })
+        );
 
-        './assets/css/images/arrow.svg',
-
-        './assets/css/images/overlay.png',
-
-        './assets/js/breakpoints.min.js',
-
-        './assets/js/browser.min.js',
-
-        './assets/js/jquery.min.js',
-        
-        './assets/js/jquery.scrollex.min.js',
-        
-        './assets/js/jquery.scrolly.min.js',
-
-        './assets/js/main.js',
-        
-        './assets/js/util.js',
-
-        './assets/js/popper.min.js',
-
-        './assets/sass/main.scss',
-
-        './assets/sass/libs/_breakpoints.scss',
-        './assets/sass/libs/_functions.scss',
-        './assets/sass/libs/_html-grid.scss',
-        './assets/sass/libs/_mixins.scss',
-        './assets/sass/libs/_vars.scss',
-        './assets/sass/libs/_vendor.scss',
-
-        './assets/webfonts/fa-brands-400.eot',
-        './assets/webfonts/fa-brands-400.svg',
-        './assets/webfonts/fa-brands-400.ttf',
-        './assets/webfonts/fa-brands-400.woff',
-        './assets/webfonts/fa-brands-400.woff2',
-        
-        './assets/webfonts/fa-regular-400.eot',
-        './assets/webfonts/fa-regular-400.svg',
-        './assets/webfonts/fa-regular-400.ttf',
-        './assets/webfonts/fa-regular-400.woff',
-        './assets/webfonts/fa-regular-400.woff2',
-
-        './icons/128.png',
-        './icons/144.png',
-        './icons/152.png',
-        './icons/167.png',
-        './icons/180.png',
-        './icons/192.png',
-        './icons/256.png',
-        './icons/512.png',
-        './icons/1024.png',
-
-        './images/bg.jpg',
-        './images/pic01.jpg',
-        './images/pic02.jpg',
-        './images/pic03.jpg',
-        './images/pic04.jpg',
-        './images/pic05.jpg',
-
-      ]))
+        if (addedResources.includes(null)) {
+          throw new Error('Algum recurso falhou ao ser adicionado ao cache');
+        }
+      })
   );
 });
 
@@ -83,7 +104,7 @@ self.addEventListener('message', function (event) {
 });
 
 self.addEventListener('fetch', function (event) {
-  //Atualizacao internet
+  // Atualizacao internet
   event.respondWith(async function () {
     try {
       return await fetch(event.request);
@@ -91,16 +112,5 @@ self.addEventListener('fetch', function (event) {
       return caches.match(event.request);
     }
   }());
-
-  //Atualizacao cache
-  /*event.respondWith(
-    caches.match(event.request)
-      .then(function (response) {
-        if (response) {
-          return response;
-        }
-        return fetch(event.request);
-      })
-  );*/
-
 });
+
